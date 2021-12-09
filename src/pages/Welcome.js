30c37545be719dfff9ac2d0828d6856d05854c78
brenolyES <brenoly.porto@upe.br>
2021-12-09 17:09:09 -0300

    estruturando pastas e arquivos

---
+++ src/pages/Welcome.js
@@ -0,0 +1,10 @@
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>boas vindas</h1>
+    </div>
+  )
+}
+
+export default Welcome
---
+++ src/routers.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Welcome from './pages/Welcome'
+import Login from './pages/Login';
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Welcome/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Routers;
