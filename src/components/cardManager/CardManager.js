b0e5a740886f181a7f01c69b528d15727f7c7ac4
brenolyES <brenoly.porto@upe.br>
2021-12-09 17:39:46 -0300

    terminada parcialmente estrutura do projeto

---
+++ src/components/cardManager/CardManager.js
@@ -0,0 +1,10 @@
+
+const CardManager = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CardManager
---
+++ src/pages/ManagerCreate.js
@@ -0,0 +1,10 @@
+
+const ManagerCreate = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ManagerCreate
