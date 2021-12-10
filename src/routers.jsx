ae008a13eb510d872a261df6145e25d5b6e5a9c0
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2021-12-10 15:13:56 -0300

    Header adicionado, routers ajustado

--- src/routers.jsx
+++ src/routers.jsx
@@ -2,19 +2,21 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Welcome from './pages/Welcome'
 import Login from './pages/Login';
 import NotFound from "./pages/NotFound";
-import DashBoard from "./pages/DashBoard";
-import Sobre from "./pages/Sobre";
-import Contato from "./pages/Contato";
+import Dashboard from './pages/Dashboard'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Header from "./components/header/Header";
 
 const Routers = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Welcome/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/dashboard' element={<DashBoard/>}/>
-        <Route path='/sobre' element={<Sobre/>}/>
-        <Route path='/contato' element={<Contato/>}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/sobre' element={<About/>}/>
+        <Route path='/contato' element={<Contact/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
