8616f4237c64e8fd9cdaaef4ab3bada52c1e59df
samuel eduardo <samueleduardo11@hotmail.com>
2021-12-11 00:39:54 -0400

    feat: adicionando pagina home (nao finalizada)

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,24 @@
-
+import logoHome from "../images/img.png"
+import logoDolar from "../images/dol.png"
 const Welcome = () => {
   return (
     <div className="container">
-       <div className="content">
-          <h1>boas vindas</h1>
-       </div>
+      <div className="content">
+        <div className="welcome">
+          <img src={logoDolar} alt="dol-img" />
+          <img src={logoHome} alt="welcome-img" />
+        </div>
+
+        <div className="welcome">
+          <h1>Onde está o Wallet?</h1>
+          <p>Onde está Wallet? é um sistema para gestão financeira de contas de serviços contratados pela DBC Company</p>
+        </div>
+      </div>
+
+      <button>Começar</button>
+
     </div>
+
   )
 }
 
