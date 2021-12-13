1dd1a09a892fcecd6b80c2f568cf17540226fae8
samuel eduardo <samueleduardo11@hotmail.com>
2021-12-12 22:32:43 -0400

    feat: adicionado read services (bt editar/excluir obs)

--- src/components/cardService/CardService.js
+++ src/components/cardService/CardService.js
@@ -1,12 +1,29 @@
 import styles from './CardService.module.css'
 import { FaEdit } from 'react-icons/fa'
-import { RiDeleteBack2Fill } from 'react-icons/ri'
-const CardService = ({ service }) => {
+import { RiDeleteBin2Fill } from 'react-icons/ri'
+import ApiWallet from '../../api'
+
+const CardService = ({ service, attList, setIdEdicao }) => {
+
+  const deleteServices = async (idServico) => {
+    try{
+      await ApiWallet.delete(`/servicos/${idServico}`)
+    }finally {
+      attList()
+    }
+  }
+
+
   return (
     <div className={styles.pageCardService}>
       <div className={styles.containerCardService}>
         <p>Nome: {service.nome}</p>
         <p>Descrição: {service.descricao}</p>
+        <p>Moeda: {service.moeda}</p>
+        <p>Valor: {service.valor}</p>
+        <p>Periodicidade: {service.periocidade}</p>
+        <button onClick={() => setIdEdicao(service.idServico)}><FaEdit /></button>
+        <button onClick={() => deleteServices(service.idServico)} ><RiDeleteBin2Fill /></button>
 
       </div>
 
