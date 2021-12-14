89ea4979b3c9ed988b4146d08811d73a2ebda090
samuel eduardo <samueleduardo11@hotmail.com>
2021-12-14 16:20:14 -0400

    feat: iniciando pagina about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,20 @@ const About = () => {
   return (
     <div className="container">
       <div className="content">
-        <h1>Sobre</h1>
+        <h1>Sobre nós</h1>
+        <p>Onde está wallet? é um sistema para gestão financeira de contas de serviços, que ajuda
+          organizar e otimizar os processos que muitas vezes são faturadas em moedas de outros países.
+          São alguns de nossos contrantes empresas como: GitLab, Slack, JIRA, Apple Developer, Google Developer,
+          typeform, New Relic, etc.
+        </p>
+
+        <div className="content">
+          <h1>Fale com a gente</h1>
+          <h2>Gostaria de saber mais?</h2>
+          <h3>Entre em contato para que possamos tirar todas as suas dúvidas</h3>
+        </div>
       </div>
+
     </div>
   )
 }
