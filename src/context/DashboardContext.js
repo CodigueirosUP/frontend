9a8c7984f7a3192eb6fe4cf7611030db9beedfcf
brenolyES <brenoly.porto@upe.br>
2021-12-17 20:40:32 -0300

    fix: bug parcialmente solucionado.

--- src/context/DashboardContext.js
+++ src/context/DashboardContext.js
@@ -5,69 +5,52 @@ const DashboardContext = createContext();
 
 const DashboardProvider = ({children}) => {
 
-  const [dataService, setDataService] = useState([]);
-  const [dataServiceValues, setDataServiceValues] = useState([]);
+  
   const [moreExpansiveService, setMoreExpansiveService] = useState();
   const [totalValueService, setTotalValueService] = useState();
 
 
-  const IdentifyUser = async (user) => {
-   
-    if( user.idUser === 1){
-      const { data } = await ApiWallet.get('/servico/list-servico');
-      console.log('data', data)
-      setDataService(data)
-      setDataServiceValues(data)
-      console.log('admin - dataServiceValues', dataServiceValues)
-      console.log('admin - dataService', dataService)
-    }else{
-      console.log('user', user)
-      setDataService(user)
-      setDataServiceValues(user.servicoDTOList)
-      console.log('gerente - dataServiceValues', dataServiceValues)
-      console.log('gerente - dataService', dataService)
-    }
-  }
  
-  const identifyMoreExpansiveValue = (user) => {
-    if( user.idUser === 1){
-      const values = dataService.map(service => service.valor);
-      const maxService = dataService.find(service => service.valor === Math.max(...values)); 
-      setMoreExpansiveService(maxService);
-    }else{
-      console.log('falta fazer do gerente')
-    }
-  }
+ 
+//   const identifyMoreExpansiveValue = (user) => {
+//     if( user.idUser === 1){
+//       const values = dataService.map(service => service.valor);
+//       const maxService = dataService.find(service => service.valor === Math.max(...values)); 
+//       setMoreExpansiveService(maxService);
+//     }else{
+//       console.log('falta fazer do gerente')
+//     }
+//   }
 
-  const identifyTotalValue = (user) => {
-    if( user.idUser === 1){
-      const values = dataService.map(service => service.valor);
-      const totalValueService = values.reduce((values, totalValues) => values + totalValues, 0); 
-      setTotalValueService(totalValueService);
-    }else{
-      console.log('falta fazer do gerente')
-    }
-  }
+//   const identifyTotalValue = (user) => {
+//     if( user.idUser === 1){
+//       const values = dataService.map(service => service.valor);
+//       const totalValueService = values.reduce((values, totalValues) => values + totalValues, 0); 
+//       setTotalValueService(totalValueService);
+//     }else{
+//       console.log('falta fazer do gerente')
+//     }
+//   }
 
-  const filterOrder = (option) => {
-    if(option === '0'){
-      setDataService(
-        dataService.sort((a, b) => {
-          return a.valor > b.valor ? -1 : (a.valor < b.valor) ? 1 : 0;
-        } )
-      )
-    }
-    else if(option === '1'){
-      setDataService(
-        dataService.sort((a, b) => {
-         return a.valor < b.valor ? -1 : (a.valor > b.valor) ? 1 : 0;
-        } )
-      )
-  }
-}
+//   const filterOrder = (option) => {
+//     if(option === '0'){
+//       setDataService(
+//         dataService.sort((a, b) => {
+//           return a.valor > b.valor ? -1 : (a.valor < b.valor) ? 1 : 0;
+//         } )
+//       )
+//     }
+//     else if(option === '1'){
+//       setDataService(
+//         dataService.sort((a, b) => {
+//          return a.valor < b.valor ? -1 : (a.valor > b.valor) ? 1 : 0;
+//         } )
+//       )
+//   }
+// }
 
   return(
-  <DashboardContext.Provider value={{filterOrder, dataService, setDataService, IdentifyUser, moreExpansiveService, identifyMoreExpansiveValue, identifyTotalValue, totalValueService}}>
+  <DashboardContext.Provider value={{}}>
     {children}
   </DashboardContext.Provider>
   )
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { FaFilter, FaSearch } from 'react-icons/fa';
 import grafico from '../images/exemplo-de-grafico-de-barras.jpg'
@@ -6,17 +6,38 @@ import { DashboardContext } from "../context/DashboardContext";
 import CardServiceDashboard from "../components/cardServiceDashboard/CardServiceDashboard";
 import { formactCurrencyIeneAndYuan, formactCurrencyReal } from "../utils/formactCurrency";
 import Graphic from "../components/graphic/Graphic";
+import ApiWallet from "../api";
 
 
 
 const Dashboard = () => {
 
   const { typeUser, getType } = useContext(AuthContext);
-  const { IdentifyUser, dataService, filterOrder, identifyMoreExpansiveValue, moreExpansiveService, totalValueService, identifyTotalValue } = useContext(DashboardContext);
+  const [dataService, setDataService] = useState([]);
+  const [dataServiceValues, setDataServiceValues] = useState([]);
+
 
   useEffect(()=>{
-    IdentifyUser(typeUser);
+    if(typeUser){
+      IdentifyUser(typeUser);
+    }
   },[typeUser])
+
+  useEffect(()=>{
+    console.log(dataService);
+    console.log(dataServiceValues);
+  },[dataService])
+
+  const IdentifyUser = async (user) => {
+    if( user.idUser === 1){
+      const { data } = await ApiWallet.get('/servico/list-servico');
+      setDataService(data)
+      setDataServiceValues(data)
+    }else{
+      setDataService(user)
+      setDataServiceValues(user.servicoDTOList)
+    }
+  }
 
   return (
     <div className="container">
@@ -29,16 +50,16 @@ const Dashboard = () => {
             <FaSearch />
           </div>
           <div >
-            <span>Gasto total</span>
+            {/* <span>Gasto total</span>
             <h1>{formactCurrencyReal(totalValueService)}</h1>
-            <span>Dezembro/21</span>
+            <span>Dezembro/21</span> */}
           </div>
           <div >
             <span>Estimativa de gasto</span>
             <h1>R$99.999,99</h1>
             <span>Janeiro/21</span>
           </div>
-          {moreExpansiveService ?
+          {/* {moreExpansiveService ?
             <div>
               <span>Serviço mais caro</span>
               <h3>{moreExpansiveService.nome}</h3>
@@ -47,16 +68,16 @@ const Dashboard = () => {
             <div>
               <h2>Não existem serviços cadastrados</h2>
             </div>
-          }
+          } */}
         </div>
         <div>
           <div className="sectionListService">
             <h3>Serviços</h3>
            <FaFilter />
-           <select name="filtro" id="filtro" onChange={(e) => filterOrder(e.target.value)}>
+           {/* <select name="filtro" id="filtro" onChange={(e) => filterOrder(e.target.value)}>
              <option value="0" >Maior → Menor</option>
              <option value="1" >Menor ← Maior</option>
-           </select>
+           </select> */}
            <div>
              {/* {dataService &&
              dataService.map(service => (
