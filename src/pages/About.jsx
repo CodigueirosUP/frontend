dfb8d08dfed09358e6cc8c932fc8b43a21c3c119
samuel eduardo <samueleduardo11@hotmail.com>
2021-12-18 15:31:15 -0400

    feat: adicioando pagina de contato e sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,35 @@
+
 const About = () => {
   return (
     <div className="container">
-      <div className="content">
-        <div className="content">
-          <h1>Sobre nós</h1>
-          <p>Onde está wallet? é um sistema para gestão financeira de contas de serviços, que ajuda
-            organizar e otimizar os processos que muitas vezes são faturadas em moedas de outros países.
-            São alguns de nossos contrantes empresas como: GitLab, Slack, JIRA, Apple Developer, Google Developer,
-            typeform, New Relic, etc.
-          </p>
-          <div >
-            <h1>Fale com a gente</h1>
-            <h2>Gostaria de saber mais?</h2>
-            <h3>Entre em contato para que possamos tirar todas as suas dúvidas</h3>
-          </div>
+      < div className="content">
+        <h1>Sobre nós</h1>
+        <p>Onde está wallet? é um sistema para gestão financeira de contas  de serviços, que ajuda
+          organizar e otimizar os processos que muitas vezes são faturadas em moedas de outros países.
+          São alguns de nossos contrantes empresas como: GitLab, Slack, JIRA, Apple Developer, Google Developer,
+          typeform, New Relic, etc.
+        </p>
+
+        <div >
+          <h1>Missao</h1>
+          <p>Organizar as informações de finanças de todos os nossos clientes e torná-las acessíveis e úteis.</p>
+        </div>
+
+        <div>
+          <h1>Visão</h1>
+          <p>Controle de finança de forma transparente.</p>
+        </div>
+
+        <div>
+          <h1>Valores</h1>
+          <nav>
+            <ul>
+              <li>O cliente no controle</li>
+              <li>Inovação</li>
+              <li>Ética</li>
+              <li>Trasnparência</li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
